3c401abed61aa07c6ca990cf682afc50fbb2ba20
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-10-06 16:06:00 -0300

    v0.2

--- main.py
+++ main.py
@@ -21,8 +21,44 @@ amarelo = (255, 232, 0)
 
 # Configurações do Jogo --------------------------------
 fim_de_jogo = False
-velocidade = 0
+velocidade = 2
 pontos = 0
+
+# Marcadores de Tamanho --------------------------------
+marcador_largura = 10
+marcador_altura = 50
+
+# Estrada e Bordas -------------------------------------
+estrada = (100, 0, 300, altura)
+marcador_borda_esquerda = (95, 0, marcador_largura, altura)
+marcador_borda_direita = (395, 0, marcador_largura, altura)
+
+# Coordenadas X das pistas -----------------------------
+pista_esquerda = 150
+pista_centro = 250
+pista_direita = 350
+pistas = [pista_esquerda, pista_centro, pista_direita]
+
+# Animação de Movimento da Pista -----------------------
+marcador_pista_y = 0
+
+class Carro(pygame.sprite.Sprite):
+    def __init__(self, imagem, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Reduzindo a imagem para caber na pista -------
+        imagem_escala = 45 / imagem.get_rect().width
+        nova_largura = imagem.get_rect().width * imagem_escala
+        nova_altura = imagem.get_rect().height * imagem_escala
+        self.image = pygame.transform.scale(imagem, (nova_largura, nova_altura))
+        
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+class CarroJogador(Carro):
+    def __init__(self, x, y):
+        pass
+    
 
 # Loop do Jogo -----------------------------------------
 relogio = pygame.time.Clock()
@@ -34,7 +70,27 @@ while rodando:
     for evento in pygame.event.get():
         if evento.type == QUIT:
             rodando = False
-            
+        
+    # Desenhando a grama -------------------------------
+    tela_jogo.fill(verde)    
+    
+    # Desenhando estrada -------------------------------
+    pygame.draw.rect(tela_jogo, cinza, estrada)
+    
+    # Desenhando bordas --------------------------------
+    pygame.draw.rect(tela_jogo, amarelo, marcador_borda_esquerda)
+    pygame.draw.rect(tela_jogo, amarelo, marcador_borda_direita)
+    
+    # Desenhando Marcadores das Pistas -----------------
+    marcador_pista_y += velocidade * 2
+    if marcador_pista_y >= marcador_altura * 2:
+        marcador_pista_y = 0
+        
+    for y in range(marcador_altura * -2, altura, marcador_altura * 2):
+        pygame.draw.rect(tela_jogo, branco, (pista_esquerda + 45, y + marcador_pista_y, marcador_largura, marcador_altura))
+        pygame.draw.rect(tela_jogo, branco, (pista_centro + 45, y + marcador_pista_y, marcador_largura, marcador_altura))
+        
+    
     pygame.display.update()
 
-oygame.quit()
+pygame.quit()
